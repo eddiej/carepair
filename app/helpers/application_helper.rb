7e9a2a9752e8cbbe8a85e0a35327c93b536eb16d
Eddie Johnston <eddie@beanstalk.ie>
2015-06-29 16:03:03 +0100

    Created welcome controller and body class/id helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+ 
+  # get an id for each controller-action. This will be inserted as an id in the body tag.
+  def bodytag_id
+    a = controller.class.to_s.underscore.gsub(/_controller$/, '').gsub(/\//, '-')
+    b = controller.action_name.underscore
+    "#{a}-#{b}".gsub(/_/, '-')
+  end
+
+  # get a class for each controller. This will be inserted as a class in the body tag.
+  def bodytag_class
+    a = controller.class.to_s.underscore.gsub(/_controller$/, '').gsub(/\//, '-')
+    "#{a}".gsub(/_/, '-')
+  end
+
 end
+
